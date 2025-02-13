0afc214dff5485236b2efaae293f566edea7e86e
mubaraktoye <toyemubarak@gmail.com>
2025-02-13 20:08:31 +0100

    test5

--- api/routes/books.py
+++ api/routes/books.py
@@ -30,6 +30,13 @@ db.books = {
         publication_year=1955,
         genre=Genre.FANTASY,
     ),
+     4: Book(
+        id=4,
+        title="The Rern of the King",
+        author="J.R.R. Tolken",
+        publication_year=1955,
+        genre=Genre.FANTASY,
+    ),
 }
 
 
@@ -61,6 +68,10 @@ async def delete_book(book_id: int) -> None:
     db.delete_book(book_id)
     return JSONResponse(status_code=status.HTTP_204_NO_CONTENT, content=None)
 
-
+@router.get("/{book_id}", response_model=Book, status_code=status.HTTP_200_OK)
+async def get_book(book_id: int):
+    book = db.books.get(book_id)
+    if not book:
+        return JSONResponse(status_code=status.HTTP_404_NOT_FOUND, content={"detail": "Book not found"})
     return book
 
